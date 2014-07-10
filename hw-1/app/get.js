4af7705b6a3b1495639c7fe99745207ea5beefb9
matthias9999 <bellmann.matthias@gmail.com>
2014-07-09 20:52:14 -0400

    first commit

--- hw-1/app/get.js
+++ hw-1/app/get.js
@@ -1,4 +1,39 @@
 var _nycda = _nycda || {};
 _nycda.get = function (item, property) {
-    return item[property];
+
+    if(item && property) {
+        if (property.indexOf(".") >= 0) {
+
+/*
+            // this one works already and completes get.js
+            var arr = property.split(".");
+            while(arr.length && (item = item[arr.shift()]));
+            return item;
+*/
+            // this is an alternative which does not yet with properties that dont exist
+
+            var arr = property.split(".");
+            var str = "item";
+            //count elements + construct return term
+            for (var i = 0; i < arr.length; i++) {
+                str = str + '["' + arr[i] + '"]';
+            }
+
+            if ((typeof eval(str)) === "undefined") {
+                return item;
+            }
+            else {
+                return eval(str);
+            }
+
+
+
+        }
+        else {
+            return item[property];
+        }
+
+        }
+        return item;
 };
+

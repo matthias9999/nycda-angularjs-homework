2d290b016c11e003e92a002f4107e7cf09ada849
matthias9999 <bellmann.matthias@gmail.com>
2014-07-23 18:07:13 -0400

    hw3 running

--- hw-3/app/scripts/controllers/search.js
+++ hw-3/app/scripts/controllers/search.js
@@ -8,6 +8,30 @@
  * Controller of the hw3App
  */
 angular.module('hw3App')
-  .controller('SearchCtrl', function ($scope) {
+  .controller('SearchCtrl', ['$scope', '$http', '$q', function ($scope, $http, $q) {
 
-  });
+    $scope.term = 'Korea';
+
+    var clearError = function(result) {
+        $scope.error = "";
+        return result;
+    };
+
+    var applyData = function(result) {
+        $scope.articles = result.data.response.docs;
+        $scope.articles.shift();
+    };
+
+    $scope.getArticles = function() {
+        $http({
+            method: 'GET',
+            url: "http://api.nytimes.com/svc/search/v2/articlesearch.json?[q="+$scope.term+"]&api-key=052861d142cf4eb7fa12bb79781fdbe1:11:69591426",
+        }).then(clearError).then(applyData);
+    };
+
+
+
+    }]);
+
+
+
